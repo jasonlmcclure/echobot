f6bd17720ba1b2151502430506b437f5f02fc66a
jasonlmcclure <jasonlmcclure@hotmail.com>
2016-05-29 13:09:51 -0500

    utter

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var bot = new builder.BotConnectorBot(botConnectorOptions);
 bot.add('/', function (session) {
     
     //respond with user's message
-    session.send("You typed " + session.message.text);
+    session.send("You uttered " + session.message.text);
 });
 
 // Setup Restify Server
